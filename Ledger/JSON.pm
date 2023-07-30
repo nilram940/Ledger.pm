47f1d1ccbbcbc6941affb82209c526d5b846c527
Dale Henderson <nilram-git@silicon-menagerie.net>
2023-07-30 16:13:55 -0500

    added early support for investment accounts

--- Ledger/JSON.pm
+++ Ledger/JSON.pm
@@ -7,9 +7,11 @@ use JSON;
 sub parsefile{
     my ($file,$callback)=@_;
     my $json=&readjson($file);
-    my $accounts=&getaccounts($json->{accounts});
+    my $securities=&getsecurities($json->{securities});
+    my $accounts=&getaccounts($json->{accounts},$securities);
 
     &addtransactions($accounts,$callback,$json->{transactions});
+    &addinvestments($accounts,$callback,$json->{investment_transactions},$securities);
     &addbalances($accounts,$callback);
 
     return 1; #(transactions=>\@trlist);
@@ -37,6 +39,7 @@ my %lia=(
 
 sub getaccounts{
     my $account_list=shift;
+    my $securities=shift;
     my $accounts={};
     foreach my $account (@{$account_list}){
         my $islia=$lia{$account->{type}}||0;
@@ -47,13 +50,19 @@ sub getaccounts{
         $ledgername=~s/([\w']+)/\u\L$1/g;
         my $balance=$account->{balances}->{current};
         $balance=-$balance if ($islia);
-        $accounts->{$account->{account_id}}={
-            ledgername => $ledgername,
-            balance => $balance,
-            islia => $islia,
-            lasttrans => 0,
+        foreach my $sec (values %{$securities}){
+            my $ledgename=$ledgername;
+            my $commodity=$sec->{ticker_symbol};
+            $ledgename.=":".$commodity
+                   unless ($commodity=~/USD/); 
+            $accounts->{$account->{account_id}}->{$commodity} = {
+                ledgername => $ledgename,
+                balance => $balance,
+                islia => $islia,
+                lasttrans => 0,
+                commodity => $commodity,
+            }
         }
-        
     }
     return $accounts;
 }
@@ -63,7 +72,7 @@ sub addtransactions{
     my $callback=shift;
     my $transactions=shift;
     foreach my $transaction (@{$transactions}){
-        my $account=$accounts->{$transaction->{account_id}};
+        my $account=$accounts->{$transaction->{account_id}}->{"CUR:USD"};
         my %tran;
         @tran{qw(quantity payee id number)}=
             @{$transaction}{qw(amount merchant_name transaction_id check_number)};
@@ -81,15 +90,56 @@ sub addtransactions{
     }
 }
 
+sub addinvestments{
+    my $accounts=shift;
+    my $callback=shift;
+    my $transactions=shift;
+    my $securities=shift;
+    foreach my $transaction (@{$transactions}){
+        my $secid=$transaction->{security_id}||"USD";
+        my $commodity=$securities->{$secid}->{ticker_symbol};
+        my $account=$accounts->{$transaction->{account_id}}->{$commodity};
+        my %tran;
+        @tran{qw(payee id)}=
+            @{$transaction}{qw(name investment_transaction_id)};
+        $tran{state}='cleared';
+        if ($commodity=~/USD/){
+            $tran{quantity}=$transaction->{amount};
+        }else{
+            @tran{qw(quantity cost)}=
+                @{$transaction}{qw(quantity amount)};
+            $tran{commodity}=$commodity;
+        }
+        $tran{date}=str2time($transaction->{date});
+        $tran{account}=$account->{ledgername};
+        my ($t,$p)=&{$callback}(\%tran);
+        if ($t && ($tran{state} eq 'cleared')){
+            $account->{lasttrans}=$tran{date}
+            if ($tran{date}>$account->{lasttrans});
+        }
+    }
+}
+
+sub getsecurities{
+    my $seclist=shift;
+    my $securities={USD =>{ticker_symbol => 'CUR:USD'}};
+    foreach my $sec (@{$seclist}){
+        $securities->{$sec->{security_id}}=$sec;
+    }
+    return $securities;
+}
+
 sub addbalances{
     my $accounts=shift;
     my $callback=shift;
-    foreach my $account (values %{$accounts}){
-        next unless $account->{lasttrans}>0;
-        my %balance;
-        @balance{qw(account date quantity cost)}=
-            (@{$account}{qw(ledgername lasttrans balance)}, 'BAL');
-        &{$callback}(\%balance);
+    foreach my $security (values %{$accounts}){
+        foreach my $account (values %{$security}){
+            next unless $account->{lasttrans}>0;
+            my %balance;
+            @balance{qw(account date quantity cost)}=
+                (@{$account}{qw(ledgername lasttrans balance)}, 'BAL');
+            &{$callback}(\%balance);
+        }
     }
 }
 
